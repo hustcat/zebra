ee7d958ac6d05066ec632d2bb9c333c1bb5d5fa3
hustcat <hustcat@gmail.com>
2014-11-18 13:20:20 +0800

    add delbr.sh

---
+++ bin/delbr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BR=br1
+HOST_IFNAME=eth1
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <bridge>"
+	exit 1
+fi
+
+BR=$1
+
+brctl delif $BR $HOST_IFNAME
+ip link set $BR down
+brctl delbr $BR
+service network restart
+
+exit 0
